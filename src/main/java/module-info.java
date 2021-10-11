6a6f1207c6cbfdbfcde4832b588024936dc361ee
Joel Johansen <dev.joejoh@gmail.com>
2021-10-11 15:40:49 +0200

    added note to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module se.iths.jojeoh.javafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    // requires java.base; laddas automatiskt in.
 
     opens se.iths.jojeoh.javafx to javafx.fxml;
     exports se.iths.jojeoh.javafx;
