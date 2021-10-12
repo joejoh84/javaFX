c5062ea62e241188ee17ae89f09ad74237190a64
Joel Johansen <dev.joejoh@gmail.com>
2021-10-12 08:48:11 +0200

    Some notes

--- src/main/java/se/iths/jojeoh/javafx/HelloApplication.java
+++ src/main/java/se/iths/jojeoh/javafx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        //scene.getStylesheets().add(getClass().getResource("stylesheet.css").getFile());
+        //scene.getStylesheets().add(getClass().getResource("stylesheet.css").getFile()); -- bättre att ladda in detta från view
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
